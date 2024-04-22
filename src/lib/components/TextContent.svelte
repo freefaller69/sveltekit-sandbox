b3d4eef5e3bd293b5c640b3f221d6f193b35742a
Michael Benson <freefaller@gmail.com>
2024-04-21 20:29:55 -0700

    add base text content component

---
+++ src/lib/components/TextContent.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let heading;
+    export let mainText;
+</script>
+
+<div class="text-content">
+    <h2>{heading}</h2>
+    <p>{mainText}</p>
+</div>
+
+<style>
+    .text-content {
+        grid-area: text;
+        max-width: 60ch;
+    }
+</style>
