164e755fec47212cdd255501a76a81acf9b3df34
Michael Benson <freefaller@gmail.com>
2024-04-21 18:45:54 -0700

    add fonts and styles to layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+    import '@fontsource-variable/exo-2'
+    import '@fontsource-variable/montserrat'
+    import '@fontsource/lato'
+    import '../styles/reset.css'
+    import '../styles/custom-props.css'
+</script>
+
+<main>
+    <slot />
+</main>
+
+<style>
+    :global(html) {
+        font-family: var(--font-primary), sans-serif;
+        background-color: var(--color-background);
+        color: var(--color-text);
+    }
+
+    :global(h1, h2, h3, h4, h5, h6) {
+        font-family: var(--font-emphatic), sans-serif;
+        font-style: italic;
+        color: var(--color-accent);
+    }
+</style>
