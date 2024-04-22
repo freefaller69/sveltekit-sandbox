ad65158867781fc5abb0446715f1863c9cfbc6be
Michael Benson <freefaller@gmail.com>
2024-04-21 20:30:21 -0700

    add alienfx component and update page.svelte

---
+++ src/lib/components/AlienFx.svelte
@@ -0,0 +1,29 @@
+<script>
+	import TextContent from './TextContent.svelte';
+	import Media from './Media.svelte';
+	import ColorPicker from './ColorPicker.svelte';
+
+	export let textContent;
+	export let mediaContent1;
+	export let mediaContent2;
+</script>
+
+<section>
+	<TextContent {...textContent} />
+	<Media {...mediaContent1} />
+	<Media {...mediaContent2} />
+	<ColorPicker />
+</section>
+
+<style>
+	section {
+		outline: 1px solid hotpink;
+        aspect-ratio: 21 / 9;
+        display: grid;
+        grid-template-areas: 'text media controls';
+        grid-template-columns: 1fr 2fr 1fr;
+        justify-items: center;
+        align-items: center;
+        padding-inline: 5rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import AlienFx from "../lib/components/AlienFx.svelte";
+
+	const textContent = {
+		heading: 'Alien FX Lighting',
+		mainText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptas voluptates, amet esse vel, magnam ducimus corrupti dolor nobis hic beatae dolore omnis repellat corporis. Praesentium doloribus dolorem rerum ipsam!'
+	}
+
+	const mediaContent1 = {
+		srcUrl: 'https://unsplash.it/400/600',
+		altText: 'A random portrait'
+	}
+
+	const mediaContent2 = {
+		srcUrl: 'https://unsplash.it/100/150',
+		altText: 'A random portrait',
+		className: 'zero-g'
+	}
+</script>
+
+<h1>SvelteKit Sandbox</h1>
+<p>This is a sandbox application for evaluating certain approaches in SvelteKit.</p>
+<AlienFx {textContent} {mediaContent1} {mediaContent2} />
