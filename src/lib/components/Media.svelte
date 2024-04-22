4dc068bfe84670bd302f43ed183f09b8d986528e
Michael Benson <freefaller@gmail.com>
2024-04-21 20:29:38 -0700

    add base color media component

---
+++ src/lib/components/Media.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let srcUrl;
+	export let altText;
+	export let className;
+</script>
+
+<div class="media">
+	<img src={srcUrl} alt={altText} class={className} />
+</div>
+
+<style>
+	.media {
+		grid-area: media;
+	}
+
+	.zero-g {
+		outline: 3px solid lawngreen;
+		animation-name: drift, float;
+		animation-duration: 15s, 6s;
+		animation-timing-function: ease-in-out;
+		animation-iteration-count: infinite;
+		animation-direction: alternate;
+	}
+
+	@keyframes float {
+		0% {
+			transform: translateY(0px);
+		}
+
+		100% {
+			transform: translateY(-25px);
+		}
+	}
+
+	@keyframes drift {
+		0%, 100% {
+			translate: 0px;
+		}
+
+		25% {
+			translate: -10px;
+		}
+
+		75% {
+			translate: 10px;
+		}
+	}
+</style>
